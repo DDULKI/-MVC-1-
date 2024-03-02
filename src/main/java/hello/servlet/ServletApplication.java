e49a1cbfa0c91af796e8521e699716c3632db238
DDULKI <124766614+DDULKI@users.noreply.github.com>
2024-03-02 17:06:00 +0900

    스프링 MVC - 구조이해 끝

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.old.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan // 자동으로 서블릿을 등록해준다.
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
+
 	}
 
+	@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+		return new SpringMemberFormControllerV1();
+	}
 }
